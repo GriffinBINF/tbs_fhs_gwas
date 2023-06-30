fcb031b91fa9e234b67dd7452508921e6f60b6b3
GriffinBINF <99134876+GriffinBINF@users.noreply.github.com>
2023-06-30 12:10:14 -0400

    Delete concat_all_autosomes.sh

--- scripts/concat_all_autosomes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#SBATCH -o ./reports/output.merge.%a.out # STDOUT
-#SBATCH --mem=80G
-#SBATCH -t 0-23:59
-
-cd /work/larylab/Griffin_Stuff/Haojie_GWAS/data/in_progress
-bcftools concat chr{1..22}.poly.imputed.vcf.gz -Oz -o  ALL.poly.imputed.vcf.gz
