634c72a616fd3c034b3d905bb1b6b80f128fe235
GriffinBINF <99134876+GriffinBINF@users.noreply.github.com>
2023-06-30 11:42:49 -0400

    Delete step0_compress_files.R

--- scripts/step0_compress_files.R
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-N=5
-for i in `seq 1 22`
-do
-((i=i%N)); ((i++==0)) && wait
-zcat input vcf.gz | bcftools sort /work/larylab/dbgap/data3/phg000835.v5.FHS_SHARE_imputed_HRC1.genotype-calls-vcf.c1/chr${i}_c1.vcf.gz -Oz -o /work/larylab/Griffin_Stuff/Haojie_GWAS/data/compressed/chr${i}.vcf.b.gz &
-
-done
